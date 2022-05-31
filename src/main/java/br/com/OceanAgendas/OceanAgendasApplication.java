e6370881796cd6ca5036fb0a967c53d7fdc88ae5
Aleff Remberto <aleff.santos@assai.com.br>
2022-05-30 21:09:08 -0300

    Commit Inicial

---
+++ src/main/java/br/com/OceanAgendas/OceanAgendasApplication.java
@@ -0,0 +1,13 @@
+package br.com.OceanAgendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceanAgendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OceanAgendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/OceanAgendas/OceanAgendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.OceanAgendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OceanAgendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
